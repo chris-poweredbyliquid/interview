e562973f9fb57973f3856eb738da1cf016b1e615
alexandra-kreidich-itechart <a.kreidzich@itechart-group.com>
2021-02-05 09:44:29 +0300

    Add factory 4 min to add

---
+++ Factories/CustomerFactory.cs
@@ -0,0 +1,33 @@
+﻿using Address = LiquidApi.Controllers.Responses.Address;
+using Customer = LiquidApi.Controllers.Responses.Customer;
+
+namespace LiquidApi.Factories
+{
+    public class CustomerFactory
+    {
+        public Address GetAddress(Address address)
+        {
+            return new Address
+            {
+                Id = address.Id,
+                Line1 = address.Line1,
+                Line2 = address.Line2,
+                City = address.City,
+                StateOrProvince = address.StateOrProvince,
+                Country = address.Country,
+                PostalCode = address.PostalCode
+            };
+        }
+
+        public Customer GetCustomer(Customer customer)
+        {
+            return new Customer
+            {
+                Id = customer.Id,
+                FirstName = customer.FirstName,
+                LastName = customer.LastName,
+                EmailAddress = customer.EmailAddress
+            };
+        }
+    }
+}
