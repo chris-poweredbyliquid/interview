92a6acaaac0b5e1cbb2f11695641f41630d87f64
Maksim Sech <Maksim.Sech@itechart-group.com>
2021-02-16 21:42:09 +0300

    Added repository methods 8 min

--- Repositories/CustomerRepository.cs
+++ Repositories/CustomerRepository.cs
@@ -2,10 +2,42 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Threading.Tasks;
+using Microsoft.EntityFrameworkCore;
+using LiquidApi.Context;
+using LiquidApi.Contracts;
+using LiquidApi.Models;
 
 namespace LiquidApi.Repositories
 {
-    public class CustomerRepository
+    public class CustomerRepository: ICustomerRepository
     {
+        private readonly LiquidApiContext _context;
+
+        public CustomerRepository(LiquidApiContext context)
+        {
+            this._context = context;
+        }
+
+        public async Task<List<Address>> GetAddressByCountry(string coutry)
+        {
+            return await _context.Addresses
+                .Where(a => coutry.Equals(a.Country, StringComparison.InvariantCultureIgnoreCase))
+                .ToListAsync();
+        }
+
+        public async Task<Customer> GetCustomerByFirstAndLastName(string firstName, string lastName)
+        {
+            return await _context.Customers
+                .FirstOrDefaultAsync(c =>
+                    firstName.Equals(c.FirstName, StringComparison.InvariantCultureIgnoreCase)
+                    && lastName.Equals(c.LastName, StringComparison.InvariantCultureIgnoreCase)
+                );
+        }
+
+        public async Task<Customer> GetCustomerById(Guid id)
+        {
+            return await _context.Customers
+                .FirstOrDefaultAsync(c => c.Id == id);
+        }
     }
 }
---
+++ Repositories/ICustomerRepository.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using LiquidApi.Models;
+
+namespace LiquidApi.Contracts
+{
+    public interface ICustomerRepository
+    {
+        Task<List<Address>> GetAddressByCountry(string country);
+
+        Task<Customer> GetCustomerByFirstAndLastName(string firstName, string lastName);
+
+        Task<Customer> GetCustomerById(Guid id);
+    }
+}
